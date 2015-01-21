f93e585eab28ff23f32e208909b20298f9d905cd
John C. Faver <johncfaver@gmail.com>
2015-01-21 12:35:25 -0500

    ignore columns with zero variance, add function for u-values

--- som.py
+++ som.py
@@ -32,6 +32,7 @@ class map():
         self.highDimension  = len(self.data[0])
         self.mapLength      = int(math.pow(nnodes,1./dimension))
         self.normalize      = normalize
+        self.ignoreColumns  = []
         self.nodes          = []
         
         #Check input data
@@ -55,30 +56,53 @@ class map():
         print "Ready to map {}-D data to {}-D representation.".format(self.highDimension, self.lowDimension)
         print "Using {} nodes.".format(self.nnodes)
         print "Training set size: {}".format(self.ndata)
+        print "Map length: {}".format(self.mapLength)
 
 
     def normalizeData(self):
         """Normalize input data by converting to z-values,
         i.e. (x-m)/s  where m is the mean and s is the standard deviation.
+        If s == 0 then we will be ignoring this descriptor in the future.
         Save the m and s values so we can transform back to original data scale.
         """
         print "Normalizing Data...",
-        self.originalData = self.data
+        self.originalData = self.data[:]
         self.normalizationParameters = []
         for d in xrange(self.highDimension):
-            dmean, dstd = meanAndStd([ x[d] for x in self.data ])
+            dmean, dstd = meanAndStd([ x[d] for x in self.originalData ])
             self.normalizationParameters.append((dmean,dstd))
-            for row in self.data:
-                row[d] = (row[d]-dmean)/dstd
+            if dstd == 0.:
+                self.ignoreColumns.append(d)
+            else:
+                for row in self.data:
+                    row[d] = (row[d]-dmean)/dstd
         print 'Done.'
+        if(len(self.ignoreColumns)>0):
+            print 'Column(s) {} do not vary and will be ignored.'.format(self.ignoreColumns)
 
     def normalizeDataPoint(self, x):
         """Scale input data point to match normalized data"""
-        return [ (x[i]-self.normalizationParameters[i][0])/self.normalizationParameters[i][1] for i in xrange(self.highDimension)] 
-    
+        x2 = x[:]
+        for d in xrange(self.highDimension):
+            if d in self.ignoreColumns:
+                continue
+            else:
+                u = self.normalizationParameters[d][0]
+                s = self.normalizationParameters[d][1]
+                x2[d] = (x[d]-u)/s
+        return x2
+
     def unnormalizeDataPoint(self, x):
         """Scale normalized data point to original input data"""
-        return [ x[i]*self.normalizationParameters[i][1]+self.normalizationParameters[i][0] for i in xrange(self.highDimension)] 
+        x2 = x[:]
+        for d in xrange(self.highDimension):
+            if d in self.ignoreColumns:
+                continue
+            else:
+                u = self.normalizationParameters[d][0]
+                s = self.normalizationParameters[d][1]
+                x2[d] = (x[d]*s)+u
+        return x2
 
     def initializeMap(self):
         #Assume a 2D map for now
@@ -109,7 +133,7 @@ class map():
             raise Exception, "Couldn't find a close node!"
         return index_min
 
-    def train(self, nsteps=100):
+    def train(self, nsteps=100, progress=False):
         """Fit the map to the data.
         Search radius in low-dimensional space will decay over time as:
                 r(t) = radius0*exp(-istep/nsteps*tscale)
@@ -119,20 +143,23 @@ class map():
                 P(t) = exp(d^2/(2*r(t)^2))
                 where d is the distance between nodes
         """
-        print 'Training map...'
+        print 'Training map with {} steps...'.format(nsteps)
         #Initial parameters
         radius0 = self.mapLength/2.
         radius0sq = math.pow(radius0,2)
         tscale  = math.log(radius0)
         learn0  = 0.1
         for istep in xrange(nsteps):
+            if progress:
+                if istep % int((nsteps/10.)) == 0:
+                    print '{}% done'.format(float(istep)/nsteps*100)
             randomDataPoint = random.choice(self.data)
             closestNodeIndex = self.findBestMatch(randomDataPoint)
             searchRadius2 =  math.pow(radius0*math.exp(-float(istep)/nsteps*tscale),2)
             learningRate = learn0*math.exp(-float(istep)/nsteps)
             if searchRadius2 <= 1.0:
                 break
-            #print 'Step {}, searchRadius={}, learningRate={}'.format(istep,searchRadius2,learningRate)
+            #print 'Step {}, searchRadius2={}, learningRate={}'.format(istep,searchRadius2,learningRate)
             for node in self.nodes:
                 nodeDistance = node.lowSquareDistanceFrom(self.nodes[closestNodeIndex].lowCoords)
                 if nodeDistance < searchRadius2:
@@ -156,26 +183,52 @@ class map():
                     for node in self.nodes:
                         f.write("Node {} at {} has value {}\n".format(node.index,node.lowCoords,self.unnormalizeDataPoint(node.highCoords)))
                 else:
+                    for node in self.nodes:
                         f.write("Node {} at {} has value {}\n".format(node.index,node.lowCoords,node.highCoords))
+
+    def updateU_values(self, radius=2.):
+        """Update u_values for each node.
+        For each node, find nodes within radius in low-dimensional space, 
+        and compute the average distance from this node to those nodes in
+        the high-dimensional space.
+        """
+        radius2 = math.pow(radius,2)
+        for node in self.nodes:
+            node.u_value = 0.
+            nneighbors = 0
+            for node2 in self.nodes:
+                if node == node2:
+                    continue
+                if any([ abs(node2.lowCoords[i]-node.lowCoords[i]) > radius for i in xrange(self.lowDimension) ]):
+                    continue
+                if squareDistance(node.lowCoords, node2.lowCoords) <= radius2:
+                    node.u_value += distance(node.highCoords, node2.highCoords)
+                    nneighbors += 1
+            node.u_value /= nneighbors
 
 
     def classify(self, x):
         """Return map coordinates for new point x"""
         assert( len(x) == self.highDimension), "Wrong dimension for input data, received {} but requires {}".format(len(x), self.highDimension)
         if self.normalize:
-            x = self.normalizeDataPoint(x)
-        closestNodeIndex = self.findBestMatch(x)
+            closestNodeIndex = self.findBestMatch(self.normalizeDataPoint(x))
+        else:
+            closestNodeIndex = self.findBestMatch(x)
+        #print "Assigned input to node {}".format(closestNodeIndex)
         return self.nodes[closestNodeIndex].lowCoords
 
 class node():
     def __init__(self, highCoords, lowCoords, index=None):
         """Each node consists of coordinates in the higher dimensional space
-        and the lower dimensional space.
+        and the lower dimensional space. Each node is also characterized by 
+        a u_value, which is the average distance between this node and its
+        low-dimensional neighbors in the high-dimensional space.
         """
         self.highCoords = highCoords
         self.highDimension = len(highCoords)
         self.lowCoords = lowCoords
         self.lowDimension = len(lowCoords)
+        self.u_value = 0.
         self.index = index
 
     def lowDistanceFrom(self, x):
